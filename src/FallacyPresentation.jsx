8248fe1925be419c5c75983defaf5bec36132d53
Zamanax <zamax27@gmail.com>
2019-12-24 23:07:10 +0100

    Begginning to write using Material-ui

--- src/FallacyPresentation.jsx
+++ src/FallacyPresentation.jsx
@@ -1,5 +1,9 @@
 import React from "react"
-import {Button} from "@material-ui/core" 
+import Button  from "@material-ui/core/Button" 
+import Card from "@material-ui/core/Card" 
+import CardContent from "@material-ui/core/CardContent" 
+import CardActions from "@material-ui/core/CardActions" 
+
 class FallacyPresentation extends React.Component {
     fallacyDesc = {
         "Cherry Picking": {
@@ -19,20 +23,22 @@ class FallacyPresentation extends React.Component {
 
     render () {
         return (
-            <div className="QuestionCard">
-                <h3>
-                {this.props.name}
-                </h3>
-                <p>
-                {this.description}
-                </p>
-                <br />
-                <div>
-                    <p style={{"textDecoration":"underline"}}>Example :</p>
-                    <p>{this.example}</p>
-                </div>
-                <Button variant="contained">Choose</Button>
-            </div>
+            <Card className="QuestionCard" color="primary">
+                <CardContent>
+                    <h3>
+                    {this.props.name}
+                    </h3>
+                    <p>
+                    {this.description}
+                    </p>
+                    <br />
+                    <div>
+                        <p style={{"textDecoration":"underline"}}>Example :</p>
+                        <p>{this.example}</p>
+                    </div>
+                </CardContent>
+                <CardActions><Button variant="contained">Choose</Button></CardActions>
+            </Card>
         )
     };
 }
