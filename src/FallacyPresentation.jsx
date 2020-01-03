5999c79fc0028ba091978050b3d28005f4346781
Zamanax <zamax27@gmail.com>
2020-01-03 13:17:48 +0100

    Début Style material-ui
    Co-authored-by: Octave <thepurpl3shad0w@gmail.com>

--- src/FallacyPresentation.jsx
+++ src/FallacyPresentation.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { styled } from "@material-ui/core/styles"
 import Button  from "@material-ui/core/Button" 
 import Card from "@material-ui/core/Card" 
 import CardContent from "@material-ui/core/CardContent" 
@@ -7,11 +8,11 @@ import CardActions from "@material-ui/core/CardActions"
 class FallacyPresentation extends React.Component {
     fallacyDesc = {
         "Cherry Picking": {
-            "Description": `Cherry Picking is about chosing very specific values inside a whole data, 
-                because the part you chose reinforce out argument, despite the fact that if you consider the whole, 
-                it says the exact opposite.`,
-            "Example" : `A governement chosing a specific month to convince that their politics decreased unemployement, 
-                despite the fact that every other part of the year, unemployement is increasing, is Cherry Picking.`
+            "Description": `Cherry picking is when you selectively pick out the evidence that backs up your point of view while disregarding the data that doesn’t.
+            For example is the above picture we can see that in order to get more vote one of the politician 
+            purposefully selected the part of the curve showing that the unemployment was down.`,
+            "Example" : `A classical example of a cherry picking fallacy: Global climate warming. 
+            Studying the last 20 years, we see not real augmentation in the global temperature, but looking at the last 100 years it is obvious.`
         },
     };
 
@@ -19,11 +20,19 @@ class FallacyPresentation extends React.Component {
         super(props);
         this.description = this.fallacyDesc[this.props.name].Description;
         this.example = this.fallacyDesc[this.props.name].Example;
+
+        this.Card = styled(Card)({
+            background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+            border: 0,
+            borderRadius: 3,
+            color: 'black',
+        })
+        ;
     };
 
+
     render () {
-        return (
-            <Card className="QuestionCard" color="primary">
+    return (<this.Card>
                 <CardContent>
                     <h3>
                     {this.props.name}
@@ -38,7 +47,7 @@ class FallacyPresentation extends React.Component {
                     </div>
                 </CardContent>
                 <CardActions><Button variant="contained">Choose</Button></CardActions>
-            </Card>
+            </this.Card>
         )
     };
 }
